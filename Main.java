fb8c9404c1c505dd9056b9d81beede05cc65858b
govinda-mahanti <gmahanti955@gmail.com>
2026-03-17 23:07:07 +0530

    add

---
+++ Main.java
@@ -0,0 +1,8 @@
+
+public class Main{
+    public static void main(String[] args) {
+        System.err.println("This is java programe");
+        System.err.println("output from java programe");
+        
+    }
+}
